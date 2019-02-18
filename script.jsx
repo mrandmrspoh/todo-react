249854880cfbde9952e205e1932e252b52ab8861
mrandmrspoh <elainel.atwork@gmail.com>
2019-02-18 19:09:37 +0800

    added remove and mark-as-complete

--- script.jsx
+++ script.jsx
@@ -1,109 +1,134 @@
-class Form extends React.Component {
+class ToDoListApp extends React.Component {
 
-    constructor(){
+    constructor() {
         super();
-        this.inputHandler = this.inputHandler.bind(this);
-        // this.state ={
-        //     input: '',
-        // }
-    }
-
-    inputHandler() {
-        let userInput = event.target.value;
-        // this.setState({input:userInput})
-        this.props.setTask(userInput);
-
-    }
-
-    render() {
-        return(
-            <div>
-                <input onChange={this.inputHandler} />
-            </div>
-        )
-    }
-
-}
-
-
-
-
-class AddList extends React.Component {
-
-    constructor(newTask, taskList) {
-        super();
-        this.addHandler = this.addHandler.bind(this);
-
-    }
-
-    addHandler(){
-        let previousTaskList = this.props.taskList
-        let newTaskList = previousTaskList.concat(this.props.newTask)
-        // this.setState({taskList=newTaskList})
-        this.props.setTaskList(newTaskList);
-    }
-
-    render() {
-        return (
-                <button onClick={this.addHandler}>Add Task
-                </button>
-        )
-    }
-
-}
-
-
-
-
-class ShowTasks extends React.Component {
-
-    render() {
-        let showTaskList = this.props.taskList.map((task,index) => {
-            return (
-                <li>{task}</li>
-            )
-        })
-
-        return (
-            <div>
-                <ul>
-                    {showTaskList}
-                </ul>
-            </div>
-        )
-    }
-
-}
-
-
-
-
-class Board extends React.Component {
-
-    constructor(){
-        super();
-        this.setTask = this.setTask.bind(this);
-        this.setTaskList = this.setTaskList.bind(this)
+        this.selectOnChange = this.selectOnChange.bind(this);
+        this.inputOnChange = this.inputOnChange.bind(this);
+        this.onClick = this.onClick.bind(this);
         this.state = {
+            action: '',
             task: '',
-            taskList: [],
+            taskList: ['123','234','456'],
+            completedList : [],
+            validation: '',
         }
     }
 
-    setTask(task){
-        this.setState({task})
+
+
+    selectOnChange(event) {
+        let userSelect = event.target.value;
+        this.setState( {action: userSelect} );
     }
 
-    setTaskList(taskList){
-        this.setState({taskList})
+
+
+    inputOnChange(event) {
+        let userInput = event.target.value;
+        this.setState( {task: userInput} );
     }
+
+
+
+    onClick() {
+        let action = this.state.action;
+        let task = this.state.task;
+        let taskList = this.state.taskList;
+        let completedList = this.state.completedList;
+        let resetTask = '';
+
+        if (action == 'Add') {
+
+            if (task.length >1 && task.length <200) {
+                let newTaskList = taskList.concat(task);
+                this.setState( {task: resetTask} );
+                this.setState( {taskList: newTaskList} );
+                this.setState( {validation: ''} )
+            } else {
+                this.setState( {validation: 'Error! Task must be more than 1 character and less than 200 characters!'} );
+            }
+
+        } else if (action == 'Remove') {
+            let taskId = parseInt(task);
+            if ( isNaN(taskId) ) {
+                this.setState( {validation: 'Error! Task Id should be a number!'} );
+            } else {
+                let removedTask = taskList.splice(taskId-1, 1);
+                this.setState( {task: resetTask} );
+                this.setState( {taskList: taskList} );
+                this.setState( {validation: ''} )
+            }
+
+        } else if (action == 'Mark as complete') {
+            let taskId = parseInt(task);
+            if ( isNaN(taskId) ) {
+                this.setState( {validation: 'Error! Task Id should be a number!'} );
+            } else {
+                let completedTask = taskList[taskId-1];
+                let removedTask = taskList.splice(taskId-1, 1);
+                let newCompletedList = completedList.concat(completedTask);
+                this.setState( {task: resetTask} );
+                this.setState( {taskList: taskList} );
+                this.setState( {completedList: newCompletedList} );
+                this.setState( {validation: ''} )
+            }
+
+        }
+
+    }
+
+
 
     render() {
-        return (
+        let action = this.state.action;
+        let taskList = this.state.taskList;
+        let completedList = this.state.completedList;
+        let showTaskList = taskList.map( (task, index) => {
+            return <li key={ 'task' + index } >{task}</li>
+        })
+        let showCompletedList = completedList.map( (task, index) => {
+            return <li key={ 'task' + index } >{task}</li>
+        })
+
+
+        let placeholder;
+        if (action == 'Add') {
+            placeholder = "input task name"
+        } else if (action == 'Remove' || action == 'Mark as complete') {
+            placeholder = 'input task id'
+        }
+
+        return(
             <div>
-                <Form setTask={this.setTask} />
-                <AddList taskList = {this.state.taskList} newTask = {this.state.task} setTaskList = {this.setTaskList} />
-                <ShowTasks taskList = {this.state.taskList} />
+
+                <div>
+                    <select onChange={this.selectOnChange}>
+                        <option >Select an action</option>
+                        <option >Add</option>
+                        <option>Remove</option>
+                        <option>Mark as complete</option>
+                    </select>
+                </div>
+                <br/>
+                <div>
+                    <input onChange={this.inputOnChange} value={this.state.task} placeholder={placeholder} />
+                    &nbsp;
+                    <button onClick={this.onClick} >Submit</button>
+                    <br/>
+                    <span>{this.state.validation}</span>
+                </div>
+                <div>
+                    <h1>Todo list</h1>
+                    <ol>
+                        {showTaskList}
+                    </ol>
+                </div>
+                <div>
+                    <h1>Completed list</h1>
+                    <ol>
+                        {showCompletedList}
+                    </ol>
+                </div>
             </div>
         )
     }
@@ -111,6 +136,4 @@ class Board extends React.Component {
 }
 
 
-
-
-ReactDOM.render(<Board/>, document.getElementById("root"))
+ReactDOM.render( <ToDoListApp />, document.getElementById('root') )
